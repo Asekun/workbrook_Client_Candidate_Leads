b7c9c922cf7076ba452b2919b0f062d6e2e6330e
olashina201 <olashina201@gmail.com>
2025-08-11 04:08:43 -0700

    update

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ app = FastAPI(
 # Add CORS middleware to allow any origin
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Allows all origins
+    allow_origins=["http://localhost:8000", "https://admin.workbrook.us"],  # Allows all origins
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
